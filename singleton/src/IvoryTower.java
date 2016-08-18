66706c1006bc87200bdaa0de447c1fb7d5b4b6b2
GuanJun <gkratos92@gmail.com>
2016-08-18 08:59:43 +0800

    singleton

---
+++ singleton/src/IvoryTower.java
@@ -0,0 +1,22 @@
+package singleton;
+
+public final class IvoryTower {
+	  /**
+	   * Static to class instance of the class.
+	   */
+	  private static final IvoryTower INSTANCE = new IvoryTower();
+
+	  /**
+	   * Private constructor so nobody can instantiate the class.
+	   */
+	  private IvoryTower() {}
+
+	  /**
+	   * To be called by user to obtain instance of the class.
+	   *
+	   * @return instance of the singleton.
+	   */
+	  public static IvoryTower getInstance() {
+	    return INSTANCE;
+	  }
+}
